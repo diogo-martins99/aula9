aed1efcef94411144df789c599e8199f76f58144
diogo-martins99 <diogofm99gmail.com>
2017-12-07 15:24:27 +0000

    Add invert.c

---
+++ invert.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define MAXSTR 500
+int main() {
+    char str[MAXSTR];
+    char diff = 'a' - 'A';
+    while (fgets(str, MAXSTR, stdin) != NULL) {
+        for (int i = 0; str[i] != '\0'; ++i) {
+            if ((str[i] >= 'a') && (str[i] <= 'z'))
+                printf("%c", str[i] - diff);
+            else if ((str[i] >= 'A') && (str[i] <= 'z'))
+				printf("%c", str[i] + diff);
+			else
+                printf("%c", str[i]);
+        }
+    }
+
+}
